7eb7e6417a9b15d856a7887a719265a2afe102db
Austin1a2b <cavalier283@gmail.com>
2021-11-26 06:45:37 +0800

    設定 靜態頁面 及 CSS

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-//load
+//設定加載模組
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+
+//載入靜態檔案
 app.use(express.static('public'))
+
+//設定使用樣板引擎
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
@@ -12,8 +16,6 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
-
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
 })
-
