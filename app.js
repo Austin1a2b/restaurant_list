1df7e79f592e90fd2d82a0067a80a09702c3c66c
Austin1a2b <cavalier283@gmail.com>
2021-11-26 18:44:21 +0800

    僅先 完成 index  頁面

--- app.js
+++ app.js
@@ -10,11 +10,18 @@ app.use(express.static('public'))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+//設定 port
 const port = 3000
 
+//提取資料庫的內容 至js 中
+const restaurantList = require('./restaurant')
+
+//index 頁面網站架構
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurantList: restaurantList.results })
 })
+
+
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
